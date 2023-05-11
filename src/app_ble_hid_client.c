c6c0feaf458e2d31bc0b90532de579401ac29392
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2023-05-11 11:21:08 +0200

    printk replaced with LOG API in app_ble_hid_client.c

--- src/app_ble_hid_client.c
+++ src/app_ble_hid_client.c
@@ -3,7 +3,6 @@
 #include <stddef.h>
 #include <errno.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
@@ -14,8 +13,12 @@
 #include <zephyr/sys/byteorder.h>
 #include <bluetooth/scan.h>
 #include <bluetooth/services/hogp.h>
-
 #include <zephyr/settings/settings.h>
+
+#include <zephyr/logging/log.h>
+
+#define LOG_LEVEL LOG_LEVEL_INF
+LOG_MODULE_REGISTER(app_ble_hid_client);
 
 static struct bt_conn *default_conn;
 static struct bt_hogp hogp;
@@ -34,7 +37,7 @@ static void trigger_callback(enum app_ble_hid_client_evt_type evt_type)
 		callback_event.type = evt_type;
 		callback_func(&callback_event);
 	} else {
-		printk("Callback not set\n");
+		LOG_ERR("Callback not set");
 	}
 }
 
@@ -47,7 +50,7 @@ static void scan_filter_match(struct bt_scan_device_info *device_info,
 	if (!filter_match->uuid.match ||
 	    (filter_match->uuid.count != 1)) {
 
-		printk("Invalid device connected\n");
+		LOG_ERR("Invalid device connected");
 
 		return;
 	}
@@ -56,14 +59,14 @@ static void scan_filter_match(struct bt_scan_device_info *device_info,
 
 	bt_addr_le_to_str(device_info->recv_info->addr, addr, sizeof(addr));
 
-	printk("Filters matched on UUID 0x%04x.\nAddress: %s connectable: %s\n",
+	LOG_DBG("Filters matched on UUID 0x%04x.Address: %s connectable: %s",
 		BT_UUID_16(uuid)->val,
 		addr, connectable ? "yes" : "no");
 }
 
 static void scan_connecting_error(struct bt_scan_device_info *device_info)
 {
-	printk("Connecting failed\n");
+	LOG_ERR("Connecting failed");
 }
 
 static void scan_connecting(struct bt_scan_device_info *device_info,
@@ -82,7 +85,7 @@ static void scan_filter_no_match(struct bt_scan_device_info *device_info,
 	if (device_info->recv_info->adv_type == BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
 		bt_addr_le_to_str(device_info->recv_info->addr, addr,
 				  sizeof(addr));
-		printk("Direct advertising received from %s\n", addr);
+		LOG_DBG("Direct advertising received from %s", addr);
 		bt_scan_stop();
 
 		err = bt_conn_le_create(device_info->recv_info->addr,
@@ -104,19 +107,19 @@ static void discovery_completed_cb(struct bt_gatt_dm *dm,
 {
 	int err;
 
-	printk("The discovery procedure succeeded\n");
+	LOG_DBG("The discovery procedure succeeded");
 
 	bt_gatt_dm_data_print(dm);
 
 	err = bt_hogp_handles_assign(dm, &hogp);
 	if (err) {
-		printk("Could not init HIDS client object, error: %d\n", err);
+		LOG_ERR("Could not init HIDS client object, error: %d", err);
 	}
 
 	err = bt_gatt_dm_data_release(dm);
 	if (err) {
-		printk("Could not release the discovery data, error "
-		       "code: %d\n", err);
+		LOG_ERR("Could not release the discovery data, error "
+		       "code: %d", err);
 	}
 
 	trigger_callback(APP_BLE_HID_CLIENT_EVT_SERVICE_DISC_COMPLETE);
@@ -125,14 +128,14 @@ static void discovery_completed_cb(struct bt_gatt_dm *dm,
 static void discovery_service_not_found_cb(struct bt_conn *conn,
 					   void *context)
 {
-	printk("The service could not be found during the discovery\n");
+	LOG_ERR("The service could not be found during the discovery");
 }
 
 static void discovery_error_found_cb(struct bt_conn *conn,
 				     int err,
 				     void *context)
 {
-	printk("The discovery procedure failed with %d\n", err);
+	LOG_ERR("The discovery procedure failed with %d", err);
 }
 
 static const struct bt_gatt_dm_cb discovery_cb = {
@@ -151,8 +154,8 @@ static void gatt_discover(struct bt_conn *conn)
 
 	err = bt_gatt_dm_start(conn, BT_UUID_HIDS, &discovery_cb, NULL);
 	if (err) {
-		printk("could not start the discovery procedure, error "
-			"code: %d\n", err);
+		LOG_ERR("could not start the discovery procedure, error "
+			"code: %d", err);
 	}
 }
 
@@ -164,7 +167,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (conn_err) {
-		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+		LOG_WRN("Failed to connect to %s (%u)", addr, conn_err);
 		if (conn == default_conn) {
 			bt_conn_unref(default_conn);
 			default_conn = NULL;
@@ -172,7 +175,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 			/* This demo doesn't require active scan */
 			err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
 			if (err) {
-				printk("Scanning failed to start (err %d)\n",
+				LOG_ERR("Scanning failed to start (err %d)",
 				       err);
 			}
 		}
@@ -180,13 +183,13 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 		return;
 	}
 
-	printk("Connected: %s\n", addr);
+	LOG_DBG("Connected: %s", addr);
 
 	trigger_callback(APP_BLE_HID_CLIENT_EVT_CONNECTED);
 
 	err = bt_conn_set_security(conn, BT_SECURITY_L2);
 	if (err) {
-		printk("Failed to set security: %d\n", err);
+		LOG_WRN("Failed to set security: %d", err);
 
 		gatt_discover(conn);
 	}
@@ -204,10 +207,10 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 		auth_conn = NULL;
 	}
 
-	printk("Disconnected: %s (reason %u)\n", addr, reason);
+	LOG_DBG("Disconnected: %s (reason %u)", addr, reason);
 
 	if (bt_hogp_assign_check(&hogp)) {
-		printk("HIDS client active - releasing");
+		LOG_DBG("HIDS client active - releasing");
 		bt_hogp_release(&hogp);
 	}
 
@@ -223,7 +226,7 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 	/* This demo doesn't require active scan */
 	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
 	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		LOG_ERR("Scanning failed to start (err %d)", err);
 	}
 }
 
@@ -235,9 +238,9 @@ static void security_changed(struct bt_conn *conn, bt_security_t level,
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (!err) {
-		printk("Security changed: %s level %u\n", addr, level);
+		LOG_DBG("Security changed: %s level %u", addr, level);
 	} else {
-		printk("Security failed: %s level %u err %d\n", addr, level,
+		LOG_ERR("Security failed: %s level %u err %d", addr, level,
 			err);
 	}
 
@@ -265,14 +268,14 @@ static void scan_init(void)
 
 	err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_UUID, BT_UUID_HIDS);
 	if (err) {
-		printk("Scanning filters cannot be set (err %d)\n", err);
+		LOG_ERR("Scanning filters cannot be set (err %d)", err);
 
 		return;
 	}
 
 	err = bt_scan_filter_enable(BT_SCAN_UUID_FILTER, false);
 	if (err) {
-		printk("Filters cannot be turned on (err %d)\n", err);
+		LOG_ERR("Filters cannot be turned on (err %d)", err);
 	}
 }
 
@@ -282,18 +285,16 @@ static uint8_t hogp_notify_cb(struct bt_hogp *hogp,
 			     const uint8_t *data)
 {
 	uint8_t size = bt_hogp_rep_size(rep);
-	uint8_t i;
 
 	if (!data) {
 		return BT_GATT_ITER_STOP;
 	}
-	printk("Notification, id: %u, size: %u, data:",
+
+	LOG_DBG("Notification, id: %u, size: %u",
 	       bt_hogp_rep_id(rep),
 	       size);
-	for (i = 0; i < size; ++i) {
-		printk(" 0x%x", data[i]);
-	}
-	printk("\n");
+	LOG_HEXDUMP_DBG(data, size, "Data: ");
+
 	return BT_GATT_ITER_CONTINUE;
 }
 
@@ -303,16 +304,13 @@ static uint8_t hogp_boot_mouse_report(struct bt_hogp *hogp,
 				     const uint8_t *data)
 {
 	uint8_t size = bt_hogp_rep_size(rep);
-	uint8_t i;
 
 	if (!data) {
 		return BT_GATT_ITER_STOP;
 	}
-	printk("Notification, mouse boot, size: %u, data:", size);
-	for (i = 0; i < size; ++i) {
-		printk(" 0x%x", data[i]);
-	}
-	printk("\n");
+	LOG_DBG("Notification, mouse boot, size: %u", size);
+	LOG_HEXDUMP_DBG(data, size, "Data: ");
+
 	return BT_GATT_ITER_CONTINUE;
 }
 
@@ -322,16 +320,13 @@ static uint8_t hogp_boot_kbd_report(struct bt_hogp *hogp,
 				   const uint8_t *data)
 {
 	uint8_t size = bt_hogp_rep_size(rep);
-	uint8_t i;
 
 	if (!data) {
 		return BT_GATT_ITER_STOP;
 	}
-	printk("Notification, keyboard boot, size: %u, data:", size);
-	for (i = 0; i < size; ++i) {
-		printk(" 0x%x", data[i]);
-	}
-	printk("\n");
+	LOG_DBG("Notification, keyboard boot, size: %u", size);
+	LOG_HEXDUMP_DBG(data, size, "Data: ");
+
 	return BT_GATT_ITER_CONTINUE;
 }
 
@@ -345,48 +340,48 @@ static void hids_on_ready(struct k_work *work)
 	int err;
 	struct bt_hogp_rep_info *rep = NULL;
 
-	printk("HIDS is ready to work\n");
+	LOG_DBG("HIDS is ready to work");
 
 	while (NULL != (rep = bt_hogp_rep_next(&hogp, rep))) {
 		if (bt_hogp_rep_type(rep) ==
 		    BT_HIDS_REPORT_TYPE_INPUT) {
-			printk("Subscribe to report id: %u\n",
+			LOG_DBG("Subscribe to report id: %u",
 			       bt_hogp_rep_id(rep));
 			err = bt_hogp_rep_subscribe(&hogp, rep,
 							   hogp_notify_cb);
 			if (err) {
-				printk("Subscribe error (%d)\n", err);
+				LOG_ERR("Subscribe error (%d)", err);
 			}
 		}
 	}
 	if (hogp.rep_boot.kbd_inp) {
-		printk("Subscribe to boot keyboard report\n");
+		LOG_DBG("Subscribe to boot keyboard report");
 		err = bt_hogp_rep_subscribe(&hogp,
 						   hogp.rep_boot.kbd_inp,
 						   hogp_boot_kbd_report);
 		if (err) {
-			printk("Subscribe error (%d)\n", err);
+			LOG_ERR("Subscribe error (%d)", err);
 		}
 	}
 	if (hogp.rep_boot.mouse_inp) {
-		printk("Subscribe to boot mouse report\n");
+		LOG_DBG("Subscribe to boot mouse report");
 		err = bt_hogp_rep_subscribe(&hogp,
 						   hogp.rep_boot.mouse_inp,
 						   hogp_boot_mouse_report);
 		if (err) {
-			printk("Subscribe error (%d)\n", err);
+			LOG_ERR("Subscribe error (%d)", err);
 		}
 	}
 }
 
 static void hogp_prep_fail_cb(struct bt_hogp *hogp, int err)
 {
-	printk("ERROR: HIDS client preparation failed!\n");
+	LOG_ERR("ERROR: HIDS client preparation failed!");
 }
 
 static void hogp_pm_update_cb(struct bt_hogp *hogp)
 {
-	printk("Protocol mode updated: %s\n",
+	LOG_DBG("Protocol mode updated: %s",
 	      bt_hogp_pm_get(hogp) == BT_HIDS_PM_BOOT ?
 	      "BOOT" : "REPORT");
 }
@@ -402,17 +397,17 @@ static const struct bt_hogp_init_params hogp_init_params = {
 static void button_bootmode(void)
 {
 	if (!bt_hogp_ready_check(&hogp)) {
-		printk("HID device not ready\n");
+		LOG_ERR("HID device not ready");
 		return;
 	}
 	int err;
 	enum bt_hids_pm pm = bt_hogp_pm_get(&hogp);
 	enum bt_hids_pm new_pm = ((pm == BT_HIDS_PM_BOOT) ? BT_HIDS_PM_REPORT : BT_HIDS_PM_BOOT);
 
-	printk("Setting protocol mode: %s\n", (new_pm == BT_HIDS_PM_BOOT) ? "BOOT" : "REPORT");
+	LOG_DBG("Setting protocol mode: %s", (new_pm == BT_HIDS_PM_BOOT) ? "BOOT" : "REPORT");
 	err = bt_hogp_pm_write(&hogp, new_pm);
 	if (err) {
-		printk("Cannot change protocol mode (err %d)\n", err);
+		LOG_ERR("Cannot change protocol mode (err %d)", err);
 	}
 }
 
@@ -420,7 +415,7 @@ static void hidc_write_cb(struct bt_hogp *hidc,
 			  struct bt_hogp_rep_info *rep,
 			  uint8_t err)
 {
-	printk("Caps lock sent\n");
+	LOG_DBG("Caps lock sent");
 }
 
 static void button_capslock(void)
@@ -429,15 +424,15 @@ static void button_capslock(void)
 	uint8_t data;
 
 	if (!bt_hogp_ready_check(&hogp)) {
-		printk("HID device not ready\n");
+		LOG_ERR("HID device not ready");
 		return;
 	}
 	if (!hogp.rep_boot.kbd_out) {
-		printk("HID device does not have Keyboard OUT report\n");
+		LOG_ERR("HID device does not have Keyboard OUT report");
 		return;
 	}
 	if (bt_hogp_pm_get(&hogp) != BT_HIDS_PM_BOOT) {
-		printk("This function works only in BOOT Report mode\n");
+		LOG_ERR("This function works only in BOOT Report mode");
 		return;
 	}
 	capslock_state = capslock_state ? 0 : 1;
@@ -447,10 +442,10 @@ static void button_capslock(void)
 				       hidc_write_cb);
 
 	if (err) {
-		printk("Keyboard data write error (err: %d)\n", err);
+		LOG_ERR("Keyboard data write error (err: %d)", err);
 		return;
 	}
-	printk("Caps lock send (val: 0x%x)\n", data);
+	LOG_DBG("Caps lock send (val: 0x%x)", data);
 }
 
 
@@ -460,14 +455,14 @@ static uint8_t capslock_read_cb(struct bt_hogp *hogp,
 			     const uint8_t *data)
 {
 	if (err) {
-		printk("Capslock read error (err: %u)\n", err);
+		LOG_ERR("Capslock read error (err: %u)", err);
 		return BT_GATT_ITER_STOP;
 	}
 	if (!data) {
-		printk("Capslock read - no data\n");
+		LOG_DBG("Capslock read - no data");
 		return BT_GATT_ITER_STOP;
 	}
-	printk("Received data (size: %u, data[0]: 0x%x)\n",
+	LOG_DBG("Received data (size: %u, data[0]: 0x%x)",
 	       bt_hogp_rep_size(rep), data[0]);
 
 	return BT_GATT_ITER_STOP;
@@ -480,11 +475,11 @@ static void capslock_write_cb(struct bt_hogp *hogp,
 {
 	int ret;
 
-	printk("Capslock write result: %u\n", err);
+	LOG_DBG("Capslock write result: %u", err);
 
 	ret = bt_hogp_rep_read(hogp, rep, capslock_read_cb);
 	if (ret) {
-		printk("Cannot read capslock value (err: %d)\n", ret);
+		LOG_ERR("Cannot read capslock value (err: %d)", ret);
 	}
 }
 
@@ -492,11 +487,11 @@ static void capslock_write_cb(struct bt_hogp *hogp,
 static void button_capslock_rsp(void)
 {
 	if (!bt_hogp_ready_check(&hogp)) {
-		printk("HID device not ready\n");
+		LOG_ERR("HID device not ready");
 		return;
 	}
 	if (!hogp.rep_boot.kbd_out) {
-		printk("HID device does not have Keyboard OUT report\n");
+		LOG_ERR("HID device does not have Keyboard OUT report");
 		return;
 	}
 	int err;
@@ -507,55 +502,11 @@ static void button_capslock_rsp(void)
 	err = bt_hogp_rep_write(&hogp, hogp.rep_boot.kbd_out, capslock_write_cb,
 				&data, sizeof(data));
 	if (err) {
-		printk("Keyboard data write error (err: %d)\n", err);
+		LOG_ERR("Keyboard data write error (err: %d)", err);
 		return;
 	}
-	printk("Caps lock send using write with response (val: 0x%x)\n", data);
+	LOG_DBG("Caps lock send using write with response (val: 0x%x)", data);
 }
-
-
-static void num_comp_reply(bool accept)
-{
-	if (accept) {
-		bt_conn_auth_passkey_confirm(auth_conn);
-		printk("Numeric Match, conn %p\n", auth_conn);
-	} else {
-		bt_conn_auth_cancel(auth_conn);
-		printk("Numeric Reject, conn %p\n", auth_conn);
-	}
-
-	bt_conn_unref(auth_conn);
-	auth_conn = NULL;
-}
-
-#if 0 // Disable button control of the HID client, this will be implemented differently
-static void button_handler(uint32_t button_state, uint32_t has_changed)
-{
-	uint32_t button = button_state & has_changed;
-
-	if (auth_conn) {
-		if (button & KEY_PAIRING_ACCEPT) {
-			num_comp_reply(true);
-		}
-
-		if (button & KEY_PAIRING_REJECT) {
-			num_comp_reply(false);
-		}
-
-		return;
-	}
-
-	if (button & KEY_BOOTMODE_MASK) {
-		button_bootmode();
-	}
-	if (button & KEY_CAPSLOCK_MASK) {
-		button_capslock();
-	}
-	if (button & KEY_CAPSLOCK_RSP_MASK) {
-		button_capslock_rsp();
-	}
-}
-#endif
 
 
 static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
@@ -575,7 +526,7 @@ static void auth_cancel(struct bt_conn *conn)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Pairing cancelled: %s\n", addr);
+	LOG_DBG("Pairing cancelled: %s", addr);
 }
 
 
@@ -585,7 +536,7 @@ static void pairing_complete(struct bt_conn *conn, bool bonded)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Pairing completed: %s, bonded: %d\n", addr, bonded);
+	LOG_DBG("Pairing completed: %s, bonded: %d", addr, bonded);
 }
 
 
@@ -595,7 +546,7 @@ static void pairing_failed(struct bt_conn *conn, enum bt_security_err reason)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Pairing failed conn: %s, reason %d\n", addr, reason);
+	LOG_ERR("Pairing failed conn: %s, reason %d", addr, reason);
 }
 
 static struct bt_conn_auth_cb conn_auth_callbacks = {
@@ -615,29 +566,27 @@ int app_ble_hid_client_init(struct app_ble_hid_client_config *config)
 
 	callback_func = config->callback;
 
-	printk("Starting Bluetooth Central HIDS example\n");
-
 	bt_hogp_init(&hogp, &hogp_init_params);
 
 	err = bt_conn_auth_cb_register(&conn_auth_callbacks);
 	if (err) {
-		printk("failed to register authorization callbacks.\n");
+		LOG_ERR("failed to register authorization callbacks.");
 		return err;
 	}
 
 	err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
 	if (err) {
-		printk("Failed to register authorization info callbacks.\n");
+		LOG_ERR("Failed to register authorization info callbacks.");
 		return err;
 	}
 
 	err = bt_enable(NULL);
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		LOG_ERR("Bluetooth init failed (err %d)", err);
 		return err;
 	}
 
-	printk("Bluetooth initialized\n");
+	LOG_DBG("Bluetooth initialized");
 
 	if (IS_ENABLED(CONFIG_SETTINGS)) {
 		settings_load();
@@ -647,11 +596,11 @@ int app_ble_hid_client_init(struct app_ble_hid_client_config *config)
 
 	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
 	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		LOG_ERR("Scanning failed to start (err %d)", err);
 		return err;
 	}
 
-	printk("Scanning successfully started\n");
+	LOG_INF("Module initialized");
 
 	return 0;
 }
